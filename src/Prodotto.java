5f6dfc69b97dce82d5b1490ed72859b08ab207eb
federicomolino <federicomolino450@gmail.com>
2025-01-28 22:59:01 +0100

    first commit

---
+++ src/Prodotto.java
@@ -0,0 +1,59 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prodotto {
+    
+    //Mi creo i vari attributi
+    public int codice;
+
+    public int codiceGenerato;
+
+    public String nome;
+
+    public String descrizione;
+
+    public double prezzo;
+
+    public double iva;
+
+    public Prodotto(){
+
+    }
+    
+    //Creo un metodo che andrà a generare un numero randomico fino a 3000
+    public int codiceRandom(int codiceRandom){
+        Random ran = new Random();
+        int max = 3000;
+        int numero = ran.nextInt(max);
+        codiceGenerato = codiceRandom + numero;
+        return codiceRandom + numero;
+    }
+
+    //All'interno sarà salvato il codice generato in precedenza
+    public int getRandomCodice(){
+        return codiceGenerato;
+    }
+
+    public double getPrezzo (double prezzoIniziale){
+        prezzo = 44.5;
+        prezzoIniziale = prezzo;
+        return prezzoIniziale;
+    }
+
+    public double getPrezzoIva (double prezzoConIVa){        
+        prezzoConIVa = 22;
+        double prezzoBase = getPrezzo(0);
+        double prezzoConIva = prezzoConIVa;
+        double prezzoConIvaAggiunta = (prezzoBase * prezzoConIva)/100;
+        double prezzoFinale = prezzoBase + prezzoConIvaAggiunta;
+        return prezzoFinale;
+    }
+
+    public void nomeEsteso(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = scan.nextLine();
+        System.out.println(" Il nome inserito è: "+ nome +"\n il codice associato è: " + getRandomCodice());
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,35 @@
+public class main {
+    public static void main(String[] args) throws Exception {
+
+        Prodotto giacca = new Prodotto();
+
+        //Richiamo il metodo che genra numero random
+        giacca.codiceRandom(0);
+        System.out.println("il codice è: " + giacca.codiceRandom(0));
+
+        //Richiamo il metodo su cui è presente il prezzo iniziale
+        giacca.getPrezzo(0);
+        System.out.println("Il prezzo iniziale è: " + giacca.getPrezzo(0));
+
+        //Richiamo il metodo che mi riporta il prezzo con aggiunta iva
+        giacca.getPrezzoIva(0);
+        System.out.println("Il prezzo con iva aggiunta è: " + giacca.getPrezzoIva(0));
+
+        //Richiamo metodo relativo alla stampa del nome e codice
+        giacca.nomeEsteso();
+
+
+        Prodotto polo = new Prodotto();
+
+        polo.codiceRandom(0);
+        System.out.println("il codice è: " + polo.codiceRandom(0));
+
+        polo.getPrezzo(0);
+        System.out.println("Il prezzo iniziale è: " + polo.getPrezzo(0));
+
+        polo.getPrezzoIva(0);
+        System.out.println("Il prezzo con iva aggiunta è: " + polo.getPrezzoIva(0));
+
+        polo.nomeEsteso();
+    }
+}
