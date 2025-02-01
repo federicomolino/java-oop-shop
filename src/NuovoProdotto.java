75235be4dd5327b17ca5cbb936b5b86f0f0256b7
federicomolino <federicomolino450@gmail.com>
2025-02-01 10:39:38 +0100

    fix: resa iva dinamica

--- src/NuovoProdotto.java
+++ src/NuovoProdotto.java
@@ -1,15 +1,18 @@
+
 public class NuovoProdotto {
     public static void main(String[] args) throws Exception {
 
         Prodotto giacca = new Prodotto();
-        //Richiamo il metodo che genra numero random
-        //giacca.codiceRandom();
         System.out.println("il codice è: " + giacca.codiceRandom());
 
         //Richiamo il metodo su cui è presente il prezzo iniziale
         giacca.prezzoIniziale();
+        
         //Stampo il valore salvato dentro il metodo getPrezzoInput
-        System.out.println("Il pre zzo iniziale è: " + giacca.getPrezzoInput());
+        System.out.println("Il prezzo iniziale è: " + giacca.getPrezzoInput());
+
+        //Richiamo per settare l'iva
+        giacca.setDefaultIva();
 
         //Richiamo il metodo che mi riporta il prezzo con aggiunta iva
         giacca.prezzoIva();
@@ -17,19 +20,5 @@ public class NuovoProdotto {
 
         //Richiamo metodo relativo alla stampa del nome e codice
         giacca.nomeEsteso();
-
-
-        Prodotto polo = new Prodotto();
-
-        polo.codiceRandom();
-        System.out.println("il codice è: " + polo.codiceRandom());
-
-        polo.prezzoIniziale();
-        System.out.println("Il prezzo iniziale è: " + polo.getPrezzoInput());
-
-        polo.prezzoIva();
-        System.out.println("Il prezzo con iva aggiunta è: " + polo.prezzoIva());
-
-        polo.nomeEsteso();
     }
 }
--- src/Prodotto.java
+++ src/Prodotto.java
@@ -24,11 +24,6 @@ public class Prodotto {
         return codice;
     }
 
-    //All'interno sarà salvato il codice generato in precedenza
-    public int getRandomCodice(){
-        return codice;
-    }
-
     public double prezzoIniziale (){
         System.out.print("Inserisci il prezzo inizile senza IVA: ");
         Scanner scan = new Scanner(System.in);
@@ -41,21 +36,23 @@ public class Prodotto {
         return prezzo;
     }
 
-    public int getDefaultIva(){
-        iva = 22;
+    public int setDefaultIva(){
+        System.out.print("Inserisci l'Iva del prodotto: ");
+        Scanner scan = new Scanner(System.in);
+        iva = scan.nextInt();
         return iva;
     }
 
     public double prezzoIva (){
-        double prezzoConIvaAggiunta = (prezzo * getDefaultIva())/100;
+        double prezzoConIvaAggiunta = (prezzo * iva)/100;
         double prezzoFinale = prezzo + prezzoConIvaAggiunta;
         return prezzoFinale;
     }
 
     public void nomeEsteso(){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Inserisci il tuo nome: ");
+        System.out.print("Inserisci il prodotto: ");
         String nomeInput = scan.nextLine();
-        System.out.println(" Il nome inserito è: "+ nomeInput +"\n\til codice associato è: " + getRandomCodice());
+        System.out.println(" Il prodotto inserito è: "+ nomeInput +"\n\til codice associato è: " + codice);
     }
 }
